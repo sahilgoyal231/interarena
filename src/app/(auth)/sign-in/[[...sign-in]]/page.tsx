2600a740e9e19d89236993f6299270cfef5405f0
Sahil <goyalsahil452@gmail.com>
2026-08-29 15:30:59 +0530

    fix: add text coloring to auth elements for dark mode and force redirect to /home

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function SignInPage() {
-  return <SignIn routing="hash" />
+  return <SignIn routing="hash" fallbackRedirectUrl="/home" forceRedirectUrl="/home" />
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function SignUpPage() {
-  return <SignUp routing="hash" />
+  return <SignUp routing="hash" fallbackRedirectUrl="/home" forceRedirectUrl="/home" />
 }
